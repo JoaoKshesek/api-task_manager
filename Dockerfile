32c6740f881bcef948647781987a32124ba6a1cb
JoaoKshesek <kshesek.joaovalter@gmail.com>
2025-06-05 08:19:06 -0300

    fix(migration): add command to run migrations

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,8 @@ WORKDIR /usr/src/app
 COPY package*.json ./
 RUN npm install
 
+RUN npx knex migrate:latest
+
 COPY . .
 
 RUN npm run build
--- src/server.ts
+++ src/server.ts
@@ -3,5 +3,5 @@ import app from './app';
 const port = Number(process.env.PORT) || 3333;
 
 app.listen(port,'0.0.0.0', () => {
-  console.log(`Server running on http://'0.0.0.0':${port}`);
+  console.log(`Server running on http://0.0.0.0:${port}`);
 });
