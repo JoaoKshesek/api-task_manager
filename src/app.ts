6ff02f7986731a82d24436ca2680ef337d8c18f0
JoaoKshesek <kshesek.joaovalter@gmail.com>
2025-06-05 10:07:19 -0300

    fix: update cors validation

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
-import express from 'express';
+import express from "express";
 import cors from "cors";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 import accountRoutes from "./routes/account.routes";
 import authRoutes from "./routes/auth.routes";
@@ -11,12 +11,26 @@ dotenv.config();
 
 const app = express();
 
-const allowedOrigins = [process.env.FRONTEND_URL || 'http://localhost:3000'];
+const allowedOrigins = [
+  process.env.FRONTEND_URL,
+  "https://web-task-manager-git-main-joaoksheseks-projects.vercel.app",
+  "https://web-task-manager-git-main-joaoksheseks-projects.vercel.app/",
+  "https://web-task-manager-ashy.vercel.app",
+  "https://web-task-manager-ashy.vercel.app/",
+];
 
-app.use(cors({
-  origin: allowedOrigins,
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
@@ -25,11 +39,10 @@ app.use("/auth", authRoutes);
 app.use("/dashboard", dashboardRoutes);
 app.use("/tasks", taskRoutes);
 
-app.get('/', async (req, res) => {
+app.get("/", async (req, res) => {
   try {
- 
   } catch (error) {
-    console.error('Erro ao conectar ao banco:', error);
+    console.error("Erro ao conectar ao banco:", error);
     res.status(500).json({ error: error });
   }
 });
